399b6140f09c8c18e9a93069096f2196f0448fd9
lucari <richard@lucari.co.uk>
2021-01-09 16:36:59 +0000

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,23 @@
 let degrees = 0
-basic.forever(function on_forever() {
-    
+basic.forever(function () {
     degrees = input.compassHeading()
     if (degrees < 22) {
         basic.showArrow(ArrowNames.North)
     } else if (degrees < 67) {
-        basic.showArrow(ArrowNames.NorthEast)
+        basic.showArrow(ArrowNames.NorthWest)
     } else if (degrees < 112) {
-        basic.showArrow(ArrowNames.East)
+        basic.showArrow(ArrowNames.West)
     } else if (degrees < 157) {
-        basic.showArrow(ArrowNames.SouthEast)
+        basic.showArrow(ArrowNames.SouthWest)
     } else if (degrees < 202) {
         basic.showArrow(ArrowNames.South)
     } else if (degrees < 247) {
-        basic.showArrow(ArrowNames.SouthWest)
+        basic.showArrow(ArrowNames.SouthEast)
     } else if (degrees < 292) {
-        basic.showArrow(ArrowNames.West)
+        basic.showArrow(ArrowNames.East)
     } else if (degrees < 337) {
-        basic.showArrow(ArrowNames.NorthWest)
+        basic.showArrow(ArrowNames.NorthEast)
     } else {
         basic.showArrow(ArrowNames.North)
     }
-    
 })
